20bc235ec7bc81a9c4580d4556ac797d78a03d74
yash7867 <yash98chaurasiya@gmail.com>
2025-07-16 20:55:42 +0530

    NewFirst

---
+++ src/components/user/Dashboard.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Dashboard = () => {
+  return (
+    <div>
+        <div>Hello Deepak</div>
+        <div>How are you?</div>
+        <p>Party kaisi thi aaj ki</p>
+        <h2>Good Night</h2>
+    </div>
+
+  )
+}
+
+export default Dashboard
